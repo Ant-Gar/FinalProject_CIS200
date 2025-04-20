aca9fdfdf5052407ad8fa782f83f8120b48b006c
Ant-Gar <roboant@umich.edu>
2025-04-19 22:35:49 -0400

    Update Blackjack.h

--- RealFinalProject/Blackjack.h
+++ RealFinalProject/Blackjack.h
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+#include <random>
+#include <chrono>
 using namespace std;
 
 enum Suit { Hearts, Diamonds, Spades, Clubs };
@@ -24,5 +27,33 @@ public:
 		else {
 			return 11;
 		}
+	}
+};
+
+class Shoe {
+private:
+	vector<Card> deck;
+public:
+	Shoe() {
+		for (int deckOfCards = 0; deckOfCards < 6; deckOfCards++) {
+			for (int shoeSuit = Hearts; shoeSuit < Clubs; shoeSuit++) {
+				for (int shoeNumber = Two; shoeNumber < Ace; shoeNumber++) {
+					deck.push_back(Card(static_cast<Suit>(shoeSuit), static_cast<Number>(shoeNumber)));
+				}
+			}
+		}
+	}
+
+	void shuffleDeck() {
+		auto seed = chrono::system_clock::now().time_since_epoch().count();
+		std::shuffle(deck.begin(), deck.end(), default_random_engine(seed));
+	}
+
+	Card deal() {
+		if (deck.size() < 100) {
+			shuffleDeck();
+		}
+		Card card = deck.front();
+		return card;
 	}
 };
